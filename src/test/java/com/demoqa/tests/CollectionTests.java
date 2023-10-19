8054c67023f98125348629ee8610a2074ade368a
Toalra <K0$k@$1mk@2950>
2023-10-19 22:16:07 +0300

    1

--- src/test/java/com/demoqa/tests/CollectionTests.java
+++ src/test/java/com/demoqa/tests/CollectionTests.java
@@ -8,8 +8,6 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static com.demoqa.tests.TestData.login;
-import static com.demoqa.tests.TestData.password;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static java.lang.String.format;
@@ -34,25 +32,42 @@ public class CollectionTests extends TestBase {
                 .extract().response();
 
         String isbn = "9781449365035";
-        String bookData = format("{\"userId\":\"cf1ca36c-551e-4db0-9768-35ea94748c81\",\"collectionOfIsbns\":[{\"isbn\":\"9781449325862\"}]}",
+        String deleteBookData = format("{\"userId\":\"%s\",\"isbn\":\"%s\"}",
                 authResponse.path("userId") , isbn);
 
-        Response addBookResponse = given()
-               //в спецификацию убрать ниже
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+               // .body(deleteBookData)
+                .queryParams("UserId", authResponse.path("userId"))
+                .when()
+                .delete("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+        String bookData = format("{\"userId\":\"%s\",\"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                //в спецификацию убрать ниже
                 .log().uri()
                 .log().method()
                 .log().body()
                 //в спецификацию убрать выше
                 .contentType(JSON)
-                .headers("", authResponse.path("token"))
+                .header("Authorization", "Bearer " + authResponse.path("token"))
                 .body(bookData)
                 .when()
                 .post("/BookStore/v1/Books")
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(201)
-                .extract().response();
+                .statusCode(201);
 
         open("/favicon.ico");
         getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
@@ -60,7 +75,6 @@ public class CollectionTests extends TestBase {
         getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
 
         open("/profile");
-        $("#userName-value").shouldHave(text(login));
-        }
-
+        $(".ReactTable").shouldHave(text("Speaking JavaScript"));
+    }
 }
