96f1dc0cf03edecdbf1763c6cc8a91aac23834cb
Toalra <K0$k@$1mk@2950>
2023-10-19 22:28:12 +0300

    1

--- src/test/java/com/demoqa/tests/CollectionTests.java
+++ src/test/java/com/demoqa/tests/CollectionTests.java
@@ -77,4 +77,68 @@ public class CollectionTests extends TestBase {
         open("/profile");
         $(".ReactTable").shouldHave(text("Speaking JavaScript"));
     }
+
+    @Test
+    void addBookToCollection_withDeleteTest() {
+        String authData = "{\"userName\":\"test1234560\",\"password\":\"Test123456@\"}";
+
+        Response authResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+
+        String isbn = "9781449365035";
+        String deleteBookData = format("{\"userId\":\"%s\",\"isbn\":\"%s\"}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(deleteBookData)
+                .when()
+                .delete("/BookStore/v1/Book")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+        String bookData = format("{\"userId\":\"%s\",\"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                //в спецификацию убрать ниже
+                .log().uri()
+                .log().method()
+                .log().body()
+                //в спецификацию убрать выше
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $(".ReactTable").shouldHave(text("Speaking JavaScript"));
+    }
 }
