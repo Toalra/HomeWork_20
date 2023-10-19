f5a81cf7c468f341666a8af066e7bd159e00553f
Toalra <K0$k@$1mk@2950>
2023-10-19 23:03:29 +0300

    1

--- src/test/java/com/demoqa/spec/LoginSpec.java
+++ src/test/java/com/demoqa/spec/LoginSpec.java
@@ -1,20 +1,18 @@
 package com.demoqa.spec;
 
-import com.demoqa.tests.TestBase;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static com.demoqa.helpers.CustomAllureListener.withCustomTemplates;
-
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 
-public class BookSpecs extends TestBase {
-    public static RequestSpecification requestBookSpec = with()
+public class LoginSpec {
+    public static RequestSpecification loginRequestSpec = with()
             .filter(withCustomTemplates())
             .log().uri()
             .log().method()
@@ -22,16 +20,9 @@ public class BookSpecs extends TestBase {
             .contentType(JSON)
             .baseUri(baseURI);
 
-    public static ResponseSpecification successResp201AddBookSpec = new ResponseSpecBuilder()
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
             .log(STATUS)
             .log(BODY)
-            .expectStatusCode(201)
+            .expectStatusCode(200)
             .build();
-
-    public static ResponseSpecification  successResp204DeleteBookSpec = new ResponseSpecBuilder()
-            .log(STATUS)
-            .log(BODY)
-            .expectStatusCode(204)
-            .build();
-
 }
